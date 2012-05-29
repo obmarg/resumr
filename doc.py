babc7871a0355a0164d80db239190aa8f3f5f4af
Graeme Coupar <grambo@grambo.me.uk>
2012-05-29 21:43:28 +0100

    Section now lets you view & set current content
    Shifted commit code out into gitutils
    Section will now allow you to view the current content, and set the
    current content.

--- doc.py
+++ doc.py
@@ -3,6 +3,7 @@ import os
 import time
 import pygit2
 from pygit2 import Repository, init_repository
+from gitutils import CommitBlob
 
 rootPath = 'data'
 
@@ -11,16 +12,43 @@ class Section(object):
     Represents a Section in a document
     '''
 
-    def __init__( self, name, headCommit ):
+    def __init__( self, name, headCommit, repo ):
         '''
         Constructor
         
         Args:
-            name:       The name of the section
+            name        The name of the section
             headCommit  The head commit of the section
+            repo        The repository object
         '''
         self.name = name
         self.headCommit = headCommit
+        self.repo = repo
+
+    def CurrentContent( self ):
+        '''
+        Returns the current content of the section
+        '''
+        oid = self.headCommit.tree[ 0 ].oid
+        blob = self.repo[ oid ]
+        return blob.data
+
+    def SetContent( self, newContent ):
+        '''
+        Adds a new version of the section
+
+        Args:
+            newContent  The new content of the section
+        '''
+        newId = CommitBlob(
+                self.repo,
+                newContent,
+                self.name,
+                'Updating section',
+                [ self.headCommit ],
+                Document.SECTION_REF_PREFIX + self.name
+                )
+        self.headCommit = self.repo[ newId ]
 
 
 class Document(object):
@@ -38,7 +66,7 @@ class Document(object):
             name:   The name of the document
             create: If true, will create a document
         '''
-        targetDir = os.path.join( rootPath, name + '.git')
+        targetDir = os.path.join( rootPath, name + '.git' )
         if create:
             # Create a bare repository
             self.repo = init_repository( targetDir, True )
@@ -85,7 +113,7 @@ class Document(object):
         Gets an iterator over the sections
         '''
         return ( 
-                Section( name, self.repo[ref.oid] ) 
+                Section( name, self.repo[ref.oid], self.repo ) 
                 for name, ref in self._SectionRefs()
                 )
        
@@ -96,29 +124,14 @@ class Document(object):
         Returns:
             The new Section object
         '''
-        signature = pygit2.Signature(
-                'Mr Name',
-                'name@domain.com',
-                time.time(), 0
-                )
-        # Need to create an empty blob,
-        # Then a tree/commit/both pointing at it
-        # Then add a reference repo.create_reference( 'name', oid )
-        # Then wrap the reference in a section and return
-        blob = self.repo.create_blob( '' )
-        builder = self.repo.TreeBuilder()
-        builder.insert( name, blob, 0 )
-        treeOid = builder.write()
-        commitId = self.repo.create_commit(
-                None, # Don't update any references just now
-                signature,
-                signature,
-                'Created section ' + name,
-                treeOid,
-                []
+        commitId = CommitBlob(
+                self.repo,
+                '',
+                name,
+                'Created section ' + name
                 )
         ref = self.repo.create_reference( 
                 self.SECTION_REF_PREFIX + name, 
                 commitId 
                 )
-        return Section( name, self.repo[ ref.oid ] )
+        return Section( name, self.repo[ ref.oid ], self.repo )
